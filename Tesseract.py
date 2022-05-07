751122114d3d4a10eb8ab8e91251b5d3da81f3b9
Sai Nishwanth Raj <sainishwanthraj@gmail.com>
2022-05-07 15:50:43 +0530

    pylint changes

--- Tesseract.py
+++ Tesseract.py
@@ -1,34 +1,32 @@
-import tkinter
-import pytesseract
-import numpy as np
-
 from logging import root
 from tkinter import *
-from PIL import ImageTk, Image 
+from PIL import ImageTk, Image
 from tkinter import filedialog
 from functools import partial
 
+import tkinter
+import pytesseract
+import numpy as np
 
 root = Tk()
 
 def open_img():
     path = openfilename()
- 
+
     img = Image.open(path)
-     
+    
     img = img.resize((250, 250), Image.ANTIALIAS)
- 
+
     img = ImageTk.PhotoImage(img)
-  
+
     panel = Label(root, image = img)
-     
+    
     panel.image = img
-    
+
     panel.grid(row = 2)
-    
-    btn = Button(root, text ='Submit', command = partial(printText, path)).grid(row = 5, columnspan = 10)
 
-
+    btn = Button(root, text ='Submit', command = partial(printText, path)).grid(
+    row = 5, columnspan = 10)
 
 def openfilename():
     filename = filedialog.askopenfilename(title ='"pen')
@@ -37,16 +35,19 @@ def openfilename():
 def printText(filename):
     img1 = np.array(Image.open(filename))
     text = pytesseract.image_to_string(img1)
+
     window = Tk()
+
     window.geometry("300x300")
     T = Text(window, height = 5, width = 52)
     l = Label(window, text = "OCR")
     l.config(font =("Courier", 14))
     l.pack()
     T.pack()
-    T.insert(tkinter.END, text)
-    window.mainloop()
     
+    T.insert(tkinter.END, text)
+    
+    window.mainloop()
 
 def main():
     root.title("Image ")
@@ -54,9 +55,10 @@ def main():
     root.geometry("350x350")
     
     root.resizable(width = True, height = True)
-    
-    btn = Button(root, text ='open image', command = open_img).grid(row = 1, columnspan = 4)
 
+    btn = Button(root, text ='open image', command = open_img).grid(
+    row = 1, columnspan = 4)
+    
     root.mainloop()
     
 if __name__ == "__main__":
